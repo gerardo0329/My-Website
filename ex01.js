313114645c9c3f6be19831cb7278b0b0810b69aa
gerardo0329 <gerardo032901@gmail.com>
2026-05-24 00:12:11 -0700

    New

---
+++ ex01.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+count = count + 1;
+count;
+
+let objectName = "water bottle";
+let objectColor = "blue";
+let objectAge = 2;
+let isUseful = true;
+
+let moods = ["sleepy", "nervous", "glowing", "dramatic"];
+
+moods[0];
+moods[1];
+moods[2];
+moods[3];
+
+let tinyThing = {
+  name: "Button Ghost",
+  species: "interface creature",
+  age: 3,
+  isVisible: true
+};
+
+tinyThing.name;
+tinyThing.species;
+tinyThing.age;
+tinyThing.isVisible;
