4dc6cf184da8d1c35c1a1a1fd3f1a40725c665e6
gerardo0329 <gerardo032901@gmail.com>
2026-05-28 00:58:49 -0700

    New

--- ex01.js
+++ ex01.js
@@ -1,28 +1,3 @@
-let count = 0;
-
-count = count + 1;
-count;
-
-let objectName = "water bottle";
-let objectColor = "blue";
-let objectAge = 2;
-let isUseful = true;
-
-let moods = ["sleepy", "nervous", "glowing", "dramatic"];
-
-moods[0];
-moods[1];
-moods[2];
-moods[3];
-
-let tinyThing = {
-  name: "Button Ghost",
-  species: "interface creature",
-  age: 3,
-  isVisible: true
-};
-
-tinyThing.name;
-tinyThing.species;
-tinyThing.age;
-tinyThing.isVisible;
+$("#btn-load").click(function () {
+  window.location.href='ex01.html';
+});
